67b27f154252762bbc44c090d0b1a83dc13027ac
Antonio Monge Jiménez <antonio.monge.jimenez@iesdonana.org>
2020-10-16 18:13:31 +0200

    sin saludar

--- index.php
+++ index.php
@@ -6,8 +6,28 @@
         <title>Hola Mundo</title> 
     </head>
     <body>
-        <form action="saluda.php" method="get">
-            <label for="nombre">Escribe tu nombre:</label>
+        <?php if (isset($_GET['nombre'])): ?>
+            <?php $nombre = trim($_GET['nombre']); ?>
+            <?php if ($nombre != ''): ?>
+                <h3>¡Hola, <?= $nombre ?>!</h3>
+            <?php else: ?>
+                <h2> Error: el nombre no puede ser vacio.</h2>
+            <?php endif ?>
+        <?php endif ?>
+
+        <?php if (isset($_GET['anyo'])): ?>
+            <?php $anyo = trim($_GET['anyo']); ?>
+            <?php if ($anyo != ''): ?>
+                <?php if (ctype_digit($anyo) && $anyo <= 9999): ?>
+                    <p>Naciste en <?= $anyo ?> y tienes <?= date('Y') - $anyo ?> años.</p>
+                <?php else: ?>
+                    <h2>Error: el año no tiene el formato adecuado</h2>
+                <?php endif ?>
+            <?php endif ?>
+        <?php endif ?>
+
+        <form action="index.php" method="get">
+            <label for="nombre">Escribe tu nombre*:</label>
             <input type="text" name="nombre" id="nombre"><br/>
             <label for="anyo">Escribe tu año de nacimiento:</label>
             <input type="text" name="anyo" id="anyo" size="10">
